2bad55ec925d09a9374ce8dccbac370ed4825c0e
Benjamin Strauch <b.strauch@fu-berlin.de>
2015-08-20 15:55:42 +0200

    Started implementing seperate filemanager class.
    This class is supposed do handle coordinating the
    incremental reading process of the BCL files.

---
+++ src/filemanager.cpp
@@ -0,0 +1,7 @@
+#include "filemanager.hpp"
+
+namespace kraken {
+	BCLFileManager::BCLFileManager(std::string basecalls_folder){
+
+	}
+}
---
+++ src/filemanager.hpp
@@ -0,0 +1,28 @@
+#ifndef FILEMANAGER_HPP
+#define FILEMANAGER_HPP
+
+#include "kraken_headers.hpp"
+#include <boost/filesystem.hpp>
+
+using namespace boost::filesystem;
+
+namespace kraken {
+
+struct TileInfo{
+	std::vector<path> tile_paths;
+	int first_tile; // first tile to process
+	int last_tile;  // last valid tile available
+};
+
+class BCLFileManager {
+public:
+	BCLFileManager(std::string basecalls_folder);
+	TileInfo getNextTile();
+
+//private:
+
+};
+
+} // namespace
+
+#endif
