f2348dc6684e994d88072ade15dc756b1f5bcd8a
Benjamin Strauch <b.strauch@fu-berlin.de>
2017-08-24 13:01:48 +0200

    Indent to Kraken convention.

--- src/krakenutil.cpp
+++ src/krakenutil.cpp
@@ -23,184 +23,179 @@
 using namespace std;
 
 namespace kraken {
-// Build a node->parent map from NCBI Taxonomy nodes.dmp file
-unordered_map<uint32_t, uint32_t> build_parent_map(string filename, unordered_map<uint32_t, string> &taxLevel_map) {
-	unordered_map<uint32_t, uint32_t> pmap;
-	uint32_t node_id, parent_id;
-	char tax[14];
-	string line;
-	std::ifstream ifs(filename.c_str());
-	if (ifs.rdstate() & std::ifstream::failbit) {
-		err(EX_NOINPUT, "error opening %s", filename.c_str());
-	}
+  // Build a node->parent map from NCBI Taxonomy nodes.dmp file
+  unordered_map<uint32_t, uint32_t> build_parent_map(string filename, unordered_map<uint32_t, string> &taxLevel_map) {
+    unordered_map<uint32_t, uint32_t> pmap;
+    uint32_t node_id, parent_id;
+    char tax[14];
+    string line;
+    std::ifstream ifs(filename.c_str());
+    if (ifs.rdstate() & std::ifstream::failbit) {
+      err(EX_NOINPUT, "error opening %s", filename.c_str());
+    }
 
-	while (ifs.good()) {
-		getline(ifs, line);
-		if (line.empty())
-			break;
-		sscanf(line.c_str(), "%d\t|\t%d\t|\t%s", &node_id, &parent_id, tax);
-		pmap[node_id] = parent_id;
+    while (ifs.good()) {
+      getline(ifs, line);
+      if (line.empty())
+        break;
+      sscanf(line.c_str(), "%d\t|\t%d\t|\t%s", &node_id, &parent_id, tax);
+      pmap[node_id] = parent_id;
 
-		taxLevel_map[node_id] = string(tax);
-		//std::cout << node_id << " " << parent_id << " " << string(tax) << "|\n";
-	}
-	pmap[1] = 0;
-	return pmap;
-}
+      taxLevel_map[node_id] = string(tax);
+      //std::cout << node_id << " " << parent_id << " " << string(tax) << "|\n";
+    }
+    pmap[1] = 0;
+    return pmap;
+  }
 
-// check if the call corresponds to at least the taxon level ("genus" etc.) required
-bool check_tax_level(uint32_t call, string level, unordered_map<uint32_t, uint32_t> & parent_map, unordered_map<uint32_t, string> &taxLevel_map){
+  // check if the call corresponds to at least the taxon level ("genus" etc.) required
+  bool check_tax_level(uint32_t call, string level, unordered_map<uint32_t, uint32_t> &parent_map,
+                       unordered_map<uint32_t, string> &taxLevel_map) {
     return false;
-	// check the taxonomic ancestors of the call
-	while ((call!=0) && (taxLevel_map[call] != level)){
-		call = parent_map[call];
-	}
+    // check the taxonomic ancestors of the call
+    while ((call != 0) && (taxLevel_map[call] != level)) {
+      call = parent_map[call];
+    }
 
-	// if we found the root of the taxonomic tree, the taxon
-	// was below the taxonomic level we wanted
-	return (call != 0);
-}
+    // if we found the root of the taxonomic tree, the taxon
+    // was below the taxonomic level we wanted
+    return (call != 0);
+  }
 
-// Return lowest common ancestor of a and b
-// LCA(0,x) = LCA(x,0) = x
-// Default ancestor is 1 (root of tree)
-uint32_t lca(map<uint32_t, uint32_t> &parent_map,
-		uint32_t a, uint32_t b)
-{
-	if (a == 0 || b == 0)
-		return a ? a : b;
+  // Return lowest common ancestor of a and b
+  // LCA(0,x) = LCA(x,0) = x
+  // Default ancestor is 1 (root of tree)
+  uint32_t lca(map<uint32_t, uint32_t> &parent_map,
+               uint32_t a, uint32_t b) {
+    if (a == 0 || b == 0)
+      return a ? a : b;
 
-	set<uint32_t> a_path;
-	while (a > 0) {
-		a_path.insert(a);
-		a = parent_map[a];
-	}
-	while (b > 0) {
-		if (a_path.count(b) > 0)
-			return b;
-		b = parent_map[b];
-	}
-	return 1;
-}
+    set<uint32_t> a_path;
+    while (a > 0) {
+      a_path.insert(a);
+      a = parent_map[a];
+    }
+    while (b > 0) {
+      if (a_path.count(b) > 0)
+        return b;
+      b = parent_map[b];
+    }
+    return 1;
+  }
 
-uint32_t lca(unordered_map<uint32_t, uint32_t> &parent_map,
-		uint32_t a, uint32_t b)
-{
-	if (a == 0 || b == 0)
-		return a ? a : b;
+  uint32_t lca(unordered_map<uint32_t, uint32_t> &parent_map,
+               uint32_t a, uint32_t b) {
+    if (a == 0 || b == 0)
+      return a ? a : b;
 
-	set<uint32_t> a_path;
-	while (a > 0) {
-		a_path.insert(a);
-		a = parent_map[a];
-	}
-	while (b > 0) {
-		if (a_path.count(b) > 0)
-			return b;
-		b = parent_map[b];
-	}
-	return 1;
-}
+    set<uint32_t> a_path;
+    while (a > 0) {
+      a_path.insert(a);
+      a = parent_map[a];
+    }
+    while (b > 0) {
+      if (a_path.count(b) > 0)
+        return b;
+      b = parent_map[b];
+    }
+    return 1;
+  }
 
 
-// Tree resolution: take all hit taxa (plus ancestors), then
-// return leaf of highest weighted leaf-to-root path.
-uint32_t resolve_tree(map<uint32_t, uint32_t> &hit_counts,
-		map<uint32_t, uint32_t> &parent_map)
-{
-	set<uint32_t> max_taxa;
-	uint32_t max_taxon = 0, max_score = 0;
-	map<uint32_t, uint32_t>::iterator it = hit_counts.begin();
+  // Tree resolution: take all hit taxa (plus ancestors), then
+  // return leaf of highest weighted leaf-to-root path.
+  uint32_t resolve_tree(map<uint32_t, uint32_t> &hit_counts,
+                        map<uint32_t, uint32_t> &parent_map) {
+    set<uint32_t> max_taxa;
+    uint32_t max_taxon = 0, max_score = 0;
+    map<uint32_t, uint32_t>::iterator it = hit_counts.begin();
 
-	// Sum each taxon's LTR path
-	while (it != hit_counts.end()) {
-		uint32_t taxon = it->first;
-		uint32_t node = taxon;
-		uint32_t score = 0;
-		while (node > 0) {
-			score += hit_counts[node];
-			node = parent_map[node];
-		}
+    // Sum each taxon's LTR path
+    while (it != hit_counts.end()) {
+      uint32_t taxon = it->first;
+      uint32_t node = taxon;
+      uint32_t score = 0;
+      while (node > 0) {
+        score += hit_counts[node];
+        node = parent_map[node];
+      }
 
-		if (score > max_score) {
-			max_taxa.clear();
-			max_score = score;
-			max_taxon = taxon;
-		}
-		else if (score == max_score) {
-			if (max_taxa.empty())
-				max_taxa.insert(max_taxon);
-			max_taxa.insert(taxon);
-		}
+      if (score > max_score) {
+        max_taxa.clear();
+        max_score = score;
+        max_taxon = taxon;
+      } else if (score == max_score) {
+        if (max_taxa.empty())
+          max_taxa.insert(max_taxon);
+        max_taxa.insert(taxon);
+      }
 
-		it++;
-	}
+      it++;
+    }
 
-	// If two LTR paths are tied for max, return LCA of all
-	if (! max_taxa.empty()) {
-		set<uint32_t>::iterator sit = max_taxa.begin();
-		max_taxon = *sit;
-		for (sit++; sit != max_taxa.end(); sit++){
-			max_taxon = lca(parent_map, max_taxon, *sit);
-		}
-	}
+    // If two LTR paths are tied for max, return LCA of all
+    if (!max_taxa.empty()) {
+      set<uint32_t>::iterator sit = max_taxa.begin();
+      max_taxon = *sit;
+      for (sit++; sit != max_taxa.end(); sit++) {
+        max_taxon = lca(parent_map, max_taxon, *sit);
+      }
+    }
 
-	return max_taxon;
-}
+    return max_taxon;
+  }
 
-// Tree resolution: take all hit taxa (plus ancestors), then
-// return leaf of highest weighted leaf-to-root path.
-uint32_t resolve_tree2(unordered_map<uint32_t, uint32_t> &hit_counts,
-		unordered_map<uint32_t, uint32_t> &parent_map)
-{
-	set<uint32_t> max_taxa;
-	uint32_t max_taxon = 0, max_score = 0;
-	unordered_map<uint32_t, uint32_t>::iterator it = hit_counts.begin();
-	std::unordered_map<uint32_t, uint32_t>::const_iterator elem;
+  // Tree resolution: take all hit taxa (plus ancestors), then
+  // return leaf of highest weighted leaf-to-root path.
+  uint32_t resolve_tree2(unordered_map<uint32_t, uint32_t> &hit_counts,
+                         unordered_map<uint32_t, uint32_t> &parent_map) {
+    set<uint32_t> max_taxa;
+    uint32_t max_taxon = 0, max_score = 0;
+    unordered_map<uint32_t, uint32_t>::iterator it = hit_counts.begin();
+    std::unordered_map<uint32_t, uint32_t>::const_iterator elem;
 
-	// Sum each taxon's LTR path
-	while (it != hit_counts.end()) {
-		uint32_t taxon = it->first;
-		uint32_t node = taxon;
-		uint32_t score = 0;
-		while (node > 0) {
-			elem = hit_counts.find(node);
-			if (elem != hit_counts.end())
-				score += elem->second;
-			node = parent_map[node];
-		}
+    // Sum each taxon's LTR path
+    while (it != hit_counts.end()) {
+      uint32_t taxon = it->first;
+      uint32_t node = taxon;
+      uint32_t score = 0;
+      while (node > 0) {
+        elem = hit_counts.find(node);
+        if (elem != hit_counts.end())
+          score += elem->second;
+        node = parent_map[node];
+      }
 
-		if (score > max_score) {
-			max_taxa.clear();
-			max_score = score;
-			max_taxon = taxon;
-		}
-		else if (score == max_score) {
-			if (max_taxa.empty())
-				max_taxa.insert(max_taxon);
-			max_taxa.insert(taxon);
-		}
+      if (score > max_score) {
+        max_taxa.clear();
+        max_score = score;
+        max_taxon = taxon;
+      } else if (score == max_score) {
+        if (max_taxa.empty())
+          max_taxa.insert(max_taxon);
+        max_taxa.insert(taxon);
+      }
 
-		it++;
-	}
+      it++;
+    }
 
-	// If two LTR paths are tied for max, return LCA of all
-	if (! max_taxa.empty()) {
-		set<uint32_t>::iterator sit = max_taxa.begin();
-		max_taxon = *sit;
-		for (sit++; sit != max_taxa.end(); sit++){
-			max_taxon = lca(parent_map, max_taxon, *sit);
-		}
-	}
+    // If two LTR paths are tied for max, return LCA of all
+    if (!max_taxa.empty()) {
+      set<uint32_t>::iterator sit = max_taxa.begin();
+      max_taxon = *sit;
+      for (sit++; sit != max_taxa.end(); sit++) {
+        max_taxon = lca(parent_map, max_taxon, *sit);
+      }
+    }
 
-	return max_taxon;
-}
+    return max_taxon;
+  }
 
-uint8_t KmerScanner::k = 0;
-uint64_t KmerScanner::kmer_mask = 0;
-uint32_t KmerScanner::mini_kmer_mask = 0;
+  uint8_t KmerScanner::k = 0;
+  uint64_t KmerScanner::kmer_mask = 0;
+  uint32_t KmerScanner::mini_kmer_mask = 0;
 
-// Create a scanner for the string over the interval [start, finish)
+  // Create a scanner for the string over the interval [start, finish)
 /*KmerScanner::KmerScanner(string &seq, size_t start, size_t finish) {
 	if (! k)
 		errx(EX_SOFTWARE, "KmerScanner created w/o setting k");
@@ -218,83 +213,88 @@ uint32_t KmerScanner::mini_kmer_mask = 0;
 		curr_pos = pos2;
 }*/
 
-// Create a scanner for the string over the interval [start, finish)
-// extend existing kmer
-KmerScanner::KmerScanner(string &seq, size_t start, size_t finish, bool extend, uint32_t last_ambig, uint64_t last_kmer) {
-	if (! k)
-		errx(EX_SOFTWARE, "KmerScanner created w/o setting k");
-	if (finish > seq.size())
-		finish = seq.size();
+  // Create a scanner for the string over the interval [start, finish)
+  // extend existing kmer
+  KmerScanner::KmerScanner(string &seq, size_t start, size_t finish, bool extend, uint32_t last_ambig,
+                           uint64_t last_kmer) {
+    if (!k)
+      errx(EX_SOFTWARE, "KmerScanner created w/o setting k");
+    if (finish > seq.size())
+      finish = seq.size();
 
-	kmer = last_kmer;
-	ambig = last_ambig;
-	str = &seq;
-	curr_pos = start;
-	pos1 = start;
-	pos2 = finish;
+    kmer = last_kmer;
+    ambig = last_ambig;
+    str = &seq;
+    curr_pos = start;
+    pos1 = start;
+    pos2 = finish;
 
-	loaded_nt = extend ? k : 0;
+    loaded_nt = extend ? k : 0;
 
-	// disable this check if we process a continued sequence
-	if (pos2 - pos1 + 1 < k && !extend){
-		curr_pos = pos2;
-	}
+    // disable this check if we process a continued sequence
+    if (pos2 - pos1 + 1 < k && !extend) {
+      curr_pos = pos2;
+    }
 
-	//std::cout << seq << " " << start << " "  << finish << " " << pos1 << " " << curr_pos << " " << loaded_nt << " " << pos2 << "\n";
-}
+    //std::cout << seq << " " << start << " "  << finish << " " << pos1 << " " << curr_pos << " " << loaded_nt << " " << pos2 << "\n";
+  }
 
-uint8_t KmerScanner::get_k() { return k; }
+  uint8_t KmerScanner::get_k() { return k; }
 
-void KmerScanner::set_k(uint8_t n) {
-	if (k)  // Only allow one setting per execution
-		return;
-	k = n;
-	kmer_mask = ~0;
-	kmer_mask >>= sizeof(kmer_mask) * 8 - (k * 2);
-	mini_kmer_mask = ~0;
-	mini_kmer_mask >>= sizeof(mini_kmer_mask) * 8 - k;
-}
+  void KmerScanner::set_k(uint8_t n) {
+    if (k)  // Only allow one setting per execution
+      return;
+    k = n;
+    kmer_mask = ~0;
+    kmer_mask >>= sizeof(kmer_mask) * 8 - (k * 2);
+    mini_kmer_mask = ~0;
+    mini_kmer_mask >>= sizeof(mini_kmer_mask) * 8 - k;
+  }
 
-uint64_t *KmerScanner::next_kmer() {
-	if (curr_pos >= pos2){
-		//std::cout << "Returning NULL - wut\n";
-		return NULL;
-	}
-	if (loaded_nt)
-		loaded_nt--;
-	while (loaded_nt < k) {
-		loaded_nt++;
-		kmer <<= 2;
-		ambig <<= 1;
-		switch ((*str)[curr_pos++]) {
-		case 'A': case 'a':
-		break;
-		case 'C': case 'c':
-			kmer |= 1;
-			break;
-		case 'G': case 'g':
-			kmer |= 2;
-			break;
-		case 'T': case 't':
-			kmer |= 3;
-			break;
-		default:
-			ambig |= 1;
-			break;
-		}
-		kmer &= kmer_mask;
-		ambig &= mini_kmer_mask;
-	}
+  uint64_t *KmerScanner::next_kmer() {
+    if (curr_pos >= pos2) {
+      //std::cout << "Returning NULL - wut\n";
+      return NULL;
+    }
+    if (loaded_nt)
+      loaded_nt--;
+    while (loaded_nt < k) {
+      loaded_nt++;
+      kmer <<= 2;
+      ambig <<= 1;
+      switch ((*str)[curr_pos++]) {
+        case 'A':
+        case 'a':
+          break;
+        case 'C':
+        case 'c':
+          kmer |= 1;
+          break;
+        case 'G':
+        case 'g':
+          kmer |= 2;
+          break;
+        case 'T':
+        case 't':
+          kmer |= 3;
+          break;
+        default:
+          ambig |= 1;
+          break;
+      }
+      kmer &= kmer_mask;
+      ambig &= mini_kmer_mask;
+    }
 
-	//std::cout << (kmer % 10) << " ";
-	return &kmer;
-}
+    //std::cout << (kmer % 10) << " ";
+    return &kmer;
+  }
 
-uint32_t KmerScanner::get_ambig() {
-	return ambig;
-}
+  uint32_t KmerScanner::get_ambig() {
+    return ambig;
+  }
 
-bool KmerScanner::ambig_kmer() {
-	return !! ambig;
-}
+  bool KmerScanner::ambig_kmer() {
+    return !!ambig;
+  }
 }
